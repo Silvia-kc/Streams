ba7365c22e3d593edc7dd0c556c0174954498da8
Silvia-kc <kacakovasilvia@gmail.com>
2025-02-11 09:05:09 +0200

    Add files via upload

---
+++ ProgramPotoci1.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+string filePath = "text.txt";
+try
+{
+    using (StreamReader reader = new StreamReader(filePath))
+    {
+        int lineNumber = 0;
+        string line;
+
+        while ((line = reader.ReadLine()) != null)
+        {
+            if (lineNumber % 2 != 0)
+            {
+                Console.WriteLine(line);
+            }
+            lineNumber++;
+        }
+    }
+}
+catch (IOException ex)
+{
+    Console.WriteLine("Error reading the file: " + ex.Message);
+}
---
+++ ProgramPotoci2.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+string inputFilePath = "text.txt";
+string outputFilePath = "output.txt";
+try
+{
+    using (StreamReader reader = new StreamReader(inputFilePath))
+    using (StreamWriter writer = new StreamWriter(outputFilePath))
+    {
+        int lineNumber = 1;
+        string line;
+
+        while ((line = reader.ReadLine()) != null)
+        {
+            writer.WriteLine($"Line {lineNumber}: {line}");
+            lineNumber++;
+        }
+    }
+    Console.WriteLine("File processing complete. Check output.txt");
+}
+catch (IOException ex)
+{
+    Console.WriteLine("Error processing the file: " + ex.Message);
+}
